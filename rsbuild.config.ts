9558bf0ce68d15195d3dc86db4b334e519fda440
kjhwert <kjhwert11@gmail.com>
2025-08-20 19:58:05 +0900

    feat: 컨텐츠 타이틀 추가

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
+  html: {
+    title: "simplicity",
+  },
+  tools: {
+    rspack: {
+      resolve: {
+        alias: {
+          "@": "/src",
+        },
+      },
+    },
+  },
   plugins: [pluginReact()],
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
 import "./App.css";
-
-const videos = [
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-behind-every-design-ux-developers.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-toss-graphic-2-0.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-lunch-with-users.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-art-of-refresh.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-legal-meets-perfect-ux.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-how-to-measure-experience.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-end-of-ux-writers.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-3-tips-no-one-tell-me-about-ai.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-last-design-tool.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-accessibility-for-everyone.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-why-nobody-uses-my-design-system.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-tosst-ai-graphic-maker.mp4",
-  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-when-ux-ignores-blind-users.mp4",
-];
+import contents from "@/contents";
 
 const App = () => {
   return (
     <main className="main">
       <article className="video-list-wrapper">
         <ul className="video-list scrollbar-hide">
-          {videos.map((video) => (
-            <li key={video}>
+          {contents.map((content) => (
+            <li key={content.intro} className="video-list-item">
               <video
                 // autoPlay muted
                 playsInline
                 loop
                 width="350"
               >
-                <source src={video} />
+                <source src={content.intro} />
               </video>
+
+              <p className="video-list-title">{content.title}</p>
             </li>
           ))}
         </ul>
