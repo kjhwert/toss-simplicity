681a117ca91251328f6d3b48bf153d97b82ececf
kjhwert <kjhwert11@gmail.com>
2025-08-20 22:33:48 +0900

    feat: summary content 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,65 @@
 import "./App.css";
+import { VIDEO_WIDTH } from "@/consts";
 import contents from "@/contents";
+import type { UIEvent } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+const INITIAL_CONTENT = Math.ceil(contents.length / 2);
 
 const App = () => {
+  const ulRef = useRef<HTMLUListElement>(null);
+  const lisRef = useRef<HTMLLIElement[]>([]);
+
+  const [selectedIndex, setSelectedIndex] = useState(INITIAL_CONTENT);
+
+  useEffect(() => {
+    ulRef.current?.scrollTo({
+      left: VIDEO_WIDTH * INITIAL_CONTENT,
+    });
+  }, []);
+
+  const handleScroll = (e: UIEvent<HTMLUListElement>) => {
+    const ul = e.target as HTMLUListElement;
+
+    const nextIndex = Math.floor(ul.scrollLeft / VIDEO_WIDTH);
+
+    const centerX = ul.scrollLeft + ul.clientWidth / 2;
+    const centerLeft = Math.floor(centerX - VIDEO_WIDTH / 2);
+    const centerRight = Math.floor(centerX + VIDEO_WIDTH / 2);
+
+    console.log(centerLeft, centerRight);
+
+    if (nextIndex !== selectedIndex) {
+      setSelectedIndex(nextIndex);
+    }
+  };
+
   return (
     <main className="main">
       <article className="video-list-wrapper">
-        <ul className="video-list scrollbar-hide">
-          {contents.map((content) => (
-            <li key={content.intro} className="video-list-item">
+        <ul
+          ref={ulRef}
+          onScroll={handleScroll}
+          className="video-list scrollbar-hide"
+        >
+          {contents.map((content, index) => (
+            <li
+              ref={(ref) => {
+                if (ref) {
+                  lisRef.current[index] = ref;
+                }
+              }}
+              key={content.intro}
+              className="video-list-item"
+            >
               <video
-                // autoPlay muted
+                autoPlay={INITIAL_CONTENT === index}
+                muted={INITIAL_CONTENT === index}
                 playsInline
                 loop
-                width="350"
+                width={VIDEO_WIDTH}
               >
                 <source src={content.intro} />
               </video>
--- src/contents.ts
+++ src/contents.ts
@@ -3,66 +3,92 @@ const contents = [
     title: "디자인 조직의\n개발자들",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-behind-every-design-ux-developers.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/behind-every-design-ux-developers.mp4",
   },
   {
     title: "토스 그래픽\n더 토스답게 만들기",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-toss-graphic-2-0.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/toss-graphic-2-0.mp4",
   },
   {
     title: "모두가 유저를\n만나는 순간까지",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-lunch-with-users.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/lunch-with-users.mp4",
   },
   {
     title: "이토록\n아름다운 새로고침",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-art-of-refresh.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/the-art-of-refresh.mp4",
   },
   {
     title: "동의 화면도\n간결해질 수 있을까",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-legal-meets-perfect-ux.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/legal-meets-perfect-ux.mp4",
   },
   {
     title: "경험을\n수치화하는 방법",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-how-to-measure-experience.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/how-to-measure-experience.mp4",
   },
   {
     title: "AI시대에\n라이터로 살아남기",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-end-of-ux-writers.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/the-end-of-ux-writers.mp4",
   },
   {
     title: "AI봇을 만들면서\n배운 3가지",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-3-tips-no-one-tell-me-about-ai.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/3-tips-no-one-tell-me-about-ai.mp4",
   },
   {
     title: "토스가 디자인 툴을\n만든 이유",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-last-design-tool.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/the-last-design-tool.mp4",
   },
   {
     title: "진짜 모두를\n위한 금융",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-accessibility-for-everyone.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/accessibility-for-everyone.mp4",
   },
   {
     title: "아무도 쓰지 않는\n디자인 시스템",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-why-nobody-uses-my-design-system.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/why-nobody-uses-my-design-system.mp4",
   },
   {
     title: "토스 그래픽을\n3초 만에 그리는 AI",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-tosst-ai-graphic-maker.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/tosst-ai-graphic-maker.mp4",
   },
   {
     title: "우리가 몰랐던\n시각 장애인의 UX",
     intro:
       "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-when-ux-ignores-blind-users.mp4",
+    summary:
+      "https://static.toss.im/assets/dp/simplicity-season4-asset/session-preview-video/when-ux-ignores-blind-users.mp4",
   },
 ];
 
