d42d1cd697dd852af430364f53e6ff5da367c75b
kjhwert <kjhwert11@gmail.com>
2025-08-20 18:47:15 +0900

    feat: video list 노출

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import './App.css';
+import "./App.css";
+
+const videos = [
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-behind-every-design-ux-developers.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-toss-graphic-2-0.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-lunch-with-users.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-art-of-refresh.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-legal-meets-perfect-ux.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-how-to-measure-experience.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-end-of-ux-writers.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-3-tips-no-one-tell-me-about-ai.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-the-last-design-tool.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-accessibility-for-everyone.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-why-nobody-uses-my-design-system.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-tosst-ai-graphic-maker.mp4",
+  "https://static.toss.im/assets/dp/simplicity-season4-asset/session-graphic-card-video/sc-when-ux-ignores-blind-users.mp4",
+];
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <main className="main">
+      <article className="video-list-wrapper">
+        <ul className="video-list scrollbar-hide">
+          {videos.map((video) => (
+            <li key={video}>
+              <video
+                // autoPlay muted
+                playsInline
+                loop
+                width="350"
+              >
+                <source src={video} />
+              </video>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </main>
   );
 };
 
